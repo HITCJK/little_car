02c097f0476156a4b42dcac1e576eefe0bd551bd
Dawn <3497450764@qq.com>
2025-06-20 20:42:54 +0800

    new

--- src/line_follow.cpp
+++ src/line_follow.cpp
@@ -5,6 +5,9 @@ void line_follow()
     static int pre_error = 0;
     static int integral = 0;
     static bool stop = false;
+    static float speed_l = 0;
+    static float speed_r = 0;
+    static int count_down = 0;
 
     float error = 0;
     float output = 0;
@@ -12,8 +15,12 @@ void line_follow()
 
     if (stop == true)
     {
-        motor_l.chang_target(0);
-        motor_r.chang_target(0);
+        return;
+    }
+
+    if (count_down > 0)
+    {
+        count_down--;
         return;
     }
 
@@ -32,32 +39,43 @@ void line_follow()
         pre_error = error;
         if (output > 0)
         {
-            motor_l.chang_target(MAX_SPEED - 0.1 * output);
-            motor_r.chang_target(-(MAX_SPEED - 0.1 * output) + output);
+            speed_l = MAX_SPEED - 0.1 * output;
+            speed_r = -(MAX_SPEED - 0.1 * output) + output;
         }
         else
         {
-            motor_l.chang_target((MAX_SPEED - 0.1 * output) + output);
-            motor_r.chang_target(-(MAX_SPEED - 0.1 * output));
+            speed_l = (MAX_SPEED - 0.1 * output) + output;
+            speed_r = -(MAX_SPEED - 0.1 * output);
         }
         break;
     case 1: // left
-        motor_l.chang_target(-MAX_SPEED);
-        motor_r.chang_target(-MAX_SPEED / 2);
+        speed_l = 0;
+        speed_r = -MAX_SPEED;
         break;
     case -1: // right
-        motor_r.chang_target(MAX_SPEED / 2);
-        motor_l.chang_target(MAX_SPEED);
+        speed_r = 0;
+        speed_l = MAX_SPEED;
         break;
     case 2: // stop
-        motor_l.chang_target(0);
-        motor_r.chang_target(0);
+        speed_l = 0;
+        speed_r = 0;
         stop = true;
         break;
     case -2: // out
-        motor_l.chang_target(-MAX_SPEED / 2);
-        motor_r.chang_target(MAX_SPEED / 2);
+        if (speed_l + speed_r > 0)
+        {
+            speed_l = MAX_SPEED;
+            speed_r = 2*MAX_SPEED;
+        }
+        else
+        {
+            speed_l = -2*MAX_SPEED;
+            speed_r = -MAX_SPEED;
+        }
+        count_down = 8;
     default:
         break;
     }
+    motor_l.chang_target(speed_l);
+    motor_r.chang_target(speed_r);
 }
